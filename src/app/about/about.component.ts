a78f5c1b1dfdfd61aa86f6ee47d6447451df1842
Kiran Anand <liteninkiran@hotmail.com>
2023-10-23 22:37:46 +0100

    009 - Learn How Observables Work Under the Hood, Build Your Own HTTP Observable

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { Observable, interval, noop } from 'rxjs';
 
 @Component({
     selector: 'about',
@@ -12,7 +13,21 @@ export class AboutComponent implements OnInit {
     }
 
     public ngOnInit(): void {
-
+        const http$ = Observable.create(observer => {
+            fetch('/api/courses')
+                .then(response => response.json())
+                .then(body => {
+                    observer.next(body);
+                    observer.complete();
+                })
+                .catch(err => {
+                    observer.error(err);
+                })
+        });
+        http$.subscribe(
+            courses => console.log(courses),
+            noop,
+            () => console.log('Complete'),
+        );
     }
-
 }
