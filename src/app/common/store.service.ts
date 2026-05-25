e14e537831f707a2fd4050b3e0eb85d98bc62549
Kiran Anand <liteninkiran@hotmail.com>
2026-05-25 20:37:34 +0100

    040 - BehaviorSubject Store - Example of a Data Modification Operation

--- src/app/common/store.service.ts
+++ src/app/common/store.service.ts
@@ -1,8 +1,10 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { Course } from '../model/course';
 import { map } from 'rxjs/operators';
 import { advancedFilter, beginnerFilter, createHttpObservable } from './util';
+import { CourseData } from '../course-dialog/course-dialog.component';
+import { fromPromise } from 'rxjs/internal/observable/fromPromise';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +23,32 @@ export class Store {
 
   public selectAdvancedCourses() {
     return this.courses$.pipe(map(advancedFilter));
+  }
+
+  public saveCourse(courseId: number, changes: Partial<CourseData>) {
+    const mappedChanges: Partial<Course> = {
+      category: changes.category,
+      longDescription: changes.longDescription,
+      description: changes.description,
+    };
+    const courses = this.subject.getValue();
+    const courseIndex = courses.findIndex((course) => course.id == courseId);
+    const newCourses = courses.slice(0);
+    const updatedCourse: Course = {
+      ...courses[courseIndex],
+      ...mappedChanges,
+    };
+    newCourses[courseIndex] = updatedCourse;
+    this.subject.next(newCourses);
+    const options = {
+      method: 'PUT',
+      body: JSON.stringify(changes),
+      headers: {
+        'content-type': 'application/json',
+      },
+    };
+    const url = `/api/courses/${courseId}`;
+    return fromPromise(fetch(url, options));
   }
 
   private getCoursesObservables() {
--- src/app/course-dialog/course-dialog.component.ts
+++ src/app/course-dialog/course-dialog.component.ts
@@ -15,11 +15,9 @@ import {
   FormControl,
 } from '@angular/forms';
 import moment from 'moment';
-import { fromEvent } from 'rxjs';
-import { exhaustMap, filter } from 'rxjs/operators';
-import { fromPromise } from 'rxjs/internal-compatibility';
+import { Store } from '../common/store.service';
 
-type CourseData = {
+export type CourseData = {
   description: string;
   category: CourseCategory;
   releasedAt: moment.Moment;
@@ -30,18 +28,7 @@ type CourseForm = {
   [K in keyof CourseData]: FormControl<CourseData[K]>;
 };
 
-type Changes = Partial<CourseData>;
-
 type SaveButton = ElementRef<HTMLButtonElement>;
-
-const getInit = (changes: Changes): RequestInit => ({
-  method: 'PUT',
-  body: JSON.stringify(changes),
-  headers: { 'content-type': 'application/json' },
-});
-
-const saveCourse = (id: number, changes: Changes) =>
-  fromPromise(fetch(`/api/courses/${id}`, getInit(changes)));
 
 @Component({
   selector: 'course-dialog',
@@ -59,39 +46,26 @@ export class CourseDialogComponent implements OnInit, AfterViewInit {
   constructor(
     private formBuilder: FormBuilder,
     private dialogRef: MatDialogRef<CourseDialogComponent>,
+    private store: Store,
     @Inject(MAT_DIALOG_DATA) course: Course,
   ) {
     this.course = course;
     this.form = this.getCourseForm();
   }
 
-  ngOnInit() {
-    this.subscribeToFormChanges();
-  }
+  ngOnInit() {}
 
-  ngAfterViewInit() {
-    this.subscribeToSaveClick();
-  }
+  ngAfterViewInit() {}
 
   close() {
     this.dialogRef.close();
   }
 
-  save() {}
-
-  subscribeToSaveClick() {
-    const save = () => saveCourse(this.course.id, this.form.value);
-    const el = this.saveButton.nativeElement;
-    const click$ = fromEvent(el, 'click').pipe(exhaustMap(save));
-    click$.subscribe();
-  }
-
-  subscribeToFormChanges() {
-    const validOnly = () => this.form.valid;
-    const save = (changes: Changes) => saveCourse(this.course.id, changes);
-    const changes$ = this.form.valueChanges;
-    const saveStream$ = changes$.pipe(filter(validOnly), exhaustMap(save));
-    saveStream$.subscribe();
+  save() {
+    this.store.saveCourse(this.course.id, this.form.value).subscribe(
+      () => this.close(),
+      (err: any) => console.log('Error', err),
+    );
   }
 
   getCourseForm() {
